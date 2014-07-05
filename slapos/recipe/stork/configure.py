b949396556749ff5d29d0215001b526806131446
walidsaad <walid.saadd@gmail.com>
2014-07-05 13:43:14 +0100

    Update configure.py

--- slapos/recipe/stork/configure.py
+++ slapos/recipe/stork/configure.py
@@ -32,26 +32,58 @@ import time
 def submitJob(args):
   """Run stork_submit (if needed) for job deployment"""
   time.sleep(10)
-  print "Check if needed to submit %s job's" % args['appname']
-  if not os.path.exists(args['sig_install']):
-    print "Nothing for install or update...Exited"
-    return
+
   # '-a', "log = out.log", '-a', "error = error.log",
   server_info=args['stork_server']+':'+args['server_port']
   launch_args = [args['submit'],server_info, args['submit_file']]
   process = subprocess.Popen(launch_args, stdout=subprocess.PIPE,
-              stderr=subprocess.STDOUT, cwd=args['appdir'])
+              stderr=subprocess.STDOUT, cwd=args['datadir'])
   result = process.communicate()[0]
   if process.returncode is None or process.returncode != 0:
     print "Failed to execute stork_submit.\nThe error was: %s" % result
-  else:
-    os.unlink(args['sig_install'])
 
 def storkStart(args):
     """Start Stork """
-    script=args['start_bin']
-    port=args['port']
-    configfile=args['configfile']
-    cmd=script+" -p "+port+" -c "+configfile
-    os.system(cmd)
-  
+    proc_args = [args['start_bin'],
+                '-p', args['port'],
+                '-c', args['configfile']
+              ]
+    
+    process = subprocess.Popen(proc_args, stderr=subprocess.STDOUT,
+                                stdout=subprocess.PIPE)
+    time.sleep(3)
+    print "\n\nStork Server started with pid %s " % process.pid
+    with open(args['pid'], 'w') as f:
+      f.write(str(process.pid))
+    result = process.communicate()[0]
+    if process.returncode is None or process.returncode != 0:
+      print "Failed to start Stork Server.\nThe error was: %s" % result
+
+def checkDownloadStatus(args):
+  "Check if all files have been downloaded! "
+  job_count = len(args['dest_list'])
+  os.chdir(args['cwd'])
+
+  while job_count > 0:
+    print "Check if %s files have been downloaded!" % job_count
+    for filename in args['dest_list']:
+      log_name = "%s.err" % filename
+      destination = args['dest_list'][filename]
+      if os.path.exists(destination):
+        job_count -= 1
+        continue
+      if os.path.exists(destination + '_part') and os.path.exists(log_name):
+        with open(log_name, 'r') as log_file:
+          if not log_file.readline().startswith('SUCCESS'):
+            with open(filename + '.out', 'r') as out_file:
+              if not out_file.readline().startswith('SUCCESS'):
+                continue
+        job_count -= 1
+        time.sleep(2)
+        os.rename(destination + '_part', destination)
+        print "File %s is correctly downloaded!" % destination
+      else:
+        print "ERROR!! Check if destination and log_files exist... %s" % destination
+    
+    time.sleep(5)
+    
